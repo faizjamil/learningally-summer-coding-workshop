54d088c46ca58dc3f6d6a04946ef7371903d78f0
Faizan Jamil <faizan34@gmail.com>
2019-06-03 21:09:21 -0400

    completed first session

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+# prints 'hello world'
+print('hello world')
+# var for name
+name = 'Faizan'
+#print 'hello' + name
+print('Hello ' + name)
+# loop and print 'hello' + name
+for i in range(0, 5):
+    if (i % 2 == 0):
+        print('hello world')
+    else: 
+        print('Hello ' + name)
